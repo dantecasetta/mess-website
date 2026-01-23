3fa29ab9216180560861567afd1ac92fd9bf896d
Dante <dante@example.com>
2026-01-23 11:18:09 -0500

    Fix static files settings for deployment

--- eventapp/settings.py
+++ eventapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-75ojryw1*$a*+pr6txp9jjd&3$*wfk4%cou8hhkq##jmeq9mti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
